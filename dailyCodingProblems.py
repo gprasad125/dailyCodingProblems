de63287ebefadd5676bd70187d2666b8573cada9
Gokul Prasad <67713757+gprasad125@users.noreply.github.com>
2021-03-13 11:05:04 -0600

    Did problem 2 - 3/13/2021

--- dailyCodingProblems.py
+++ dailyCodingProblems.py
@@ -12,3 +12,24 @@ def equal(lst, k):
         return True
     else:
         return False
+
+#2. (3-13-2021)
+# Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i (NO DIVISION ALLOWED!)
+def multiplyList(lst):
+
+    if len(lst) == 0:
+        return 0
+
+    def recursiveMultiply(lst):
+        if len(lst) == 1:
+            return lst[0]
+
+        firstTerm = lst[0]
+        rest = lst[1:]
+
+        return firstTerm * recursiveMultiply(rest)
+
+    totalProduct = recursiveMultiply(lst)
+    answer = [int(totalProduct * (term ** -1)) for term in lst]
+
+    return answer
